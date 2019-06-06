16819810191afc1777434f408f5479968ce4e5b5
Anshul Khattar <anshul070300@gmail.com>
2019-06-06 10:35:28 +0530

    Add files via upload

---
+++ CodeChef_Flatland.cpp
@@ -0,0 +1,23 @@
+
+### 		CODECHEF  		###
+### Problem Name: FlatLand  ###
+### Problem Code: ICL1902  ###
+
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n,count=0;
+		cin>>n;
+		while(n>0){
+			n=n-(int(sqrt(n))*int(sqrt(n)));
+			count++;
+		}
+		cout<<count<<"\n";
+	}
+	return 0;
+}
