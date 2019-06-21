ebd689f9d1b3dd8863bef99350e4b485f7c5468d
Anshul Khattar <anshul070300@gmail.com>
2019-06-21 11:39:27 +0530

    Add files via upload

---
+++ Codechef_Interactive.py
@@ -0,0 +1,11 @@
+# CODECHEF
+# Chef and Interactive Contests
+# CHFINTRO
+
+n,r=map(int,input().split())
+for _ in range(n):
+	r1=int(input())
+	if r1>=r:
+		print("Good boi")
+	else:
+		print("Bad boi")
