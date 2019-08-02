96f8a90619562ab68fd8c5a131d9239bca63fdf9
Anshul <anshul070300@gmail.com>
2019-08-02 16:36:32 +0530

    New problem added

---
+++ Codechef_Football.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+public class Codechef_Football {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int t;
+		t=scan.nextInt();
+		while(t--!=0) {
+			int n,i,j=0,score=0;
+			n=scan.nextInt();
+			Integer a[]=new Integer[n];
+			Integer b[]=new Integer[n];
+			List<Integer> score_list=new ArrayList<Integer>();
+			for(i=0;i<n;i++) {
+				a[i]=scan.nextInt();
+				
+				}
+			for(i=0;i<n;i++) {
+				b[i]=scan.nextInt();
+				
+				}
+			while(j<n) {
+				score=(a[j]*20)-(b[j]*10);
+				if(score<0)	score=0;
+				score_list.add(score);
+				j++;
+			}
+			System.out.println(Collections.max(score_list));
+		}
+	}
+
+}
